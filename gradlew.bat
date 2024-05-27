5210c4dd0d7b63053b81996d1a3a7fddd601b490
kunu247 <63924966+kunu247@users.noreply.github.com>
2024-05-27 18:09:09 +0530

    Directories created by kunal on 27 May 2024

---
+++ gradlew.bat
@@ -0,0 +1,16 @@
+@if "%DEBUG%" == "" @echo off
+@rem #########################################################
+@rem 
+@rem Gradle Startup script for Windows
+@rem 
+@rem #########################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%" == "Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+
